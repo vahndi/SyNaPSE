91fcc565ac912db889f6d5eedc1b9049a2685fab
Vahndi Minah <vahndi.minah@gmail.com>
2015-12-17 17:40:07 -0500

    added common subviews for ensemble models and some code in element_generator.py to find common groups of input arguments

--- flow_elements/templates/element_generator.py
+++ flow_elements/templates/element_generator.py
@@ -39,16 +39,19 @@ def inspect_element_args(spreadsheet_name, sheet_name):
             else:
                 argCount[arg_val[0]] = 1
     
-    print argCount
+    for arg in sorted(argCount.keys()):
+        print arg, argCount[arg]
+
     
     # create a new dataframe showing which arguments are in which model
     model_names = list(df['Model'].unique())
     arg_names = argCount.keys()
-    models_args_dict = {}    
+    models_args_dict = {}
     for model_name in model_names:
         model_dict = {}
         args_vals = df[df['Model'] == model_name].iloc[0]['Args']
-        lst_args_vals = [av.split('=') for av in args_vals.encode('utf8').split(',')]
+        lst_args_vals = [av.split('=') 
+                         for av in args_vals.encode('utf8').split(',')]
         model_args = [av[0].strip().lstrip('\xc2\xa0') for av in lst_args_vals]
         for arg_name in arg_names:
             if arg_name in model_args:
@@ -60,8 +63,42 @@ def inspect_element_args(spreadsheet_name, sheet_name):
     df_grid = pd.DataFrame(models_args_dict)
     sns.heatmap(df_grid, linewidths=.5)
     
-    
     # find pairs of arguments which are always present together in models
+    always_together_pairs = []
+    for a1 in arg_names:
+        for a2 in arg_names:
+            if a1 != a2:
+                always_together = True
+                for m in model_names:
+                    if models_args_dict[m][a1] != models_args_dict[m][a2]:
+                        always_together = False
+                if always_together:
+                    always_together_pairs.append(tuple(sorted([a1, a2])))
+    
+    at_set = set(always_together_pairs)
+    at_list = sorted(list(at_set))
+    # merge tuples
+    merges_made = True
+    while merges_made:
+        merges_made = False
+        new_list = []
+        for at in at_list:
+            merge_made = False
+            for i, new_at in enumerate(new_list):
+                if len(set(at).intersection(new_at)) > 0:
+                    new_list[i] = tuple(
+                                    sorted(list(set(at).union(new_at)))
+                                    )
+                    merge_made = True
+                    merges_made = True
+                    break
+            if not merge_made:
+                new_list.append(at)
+                
+        at_list = new_list
+
+    for at in at_list:
+        print at
     
 
 if __name__ == '__main__':
