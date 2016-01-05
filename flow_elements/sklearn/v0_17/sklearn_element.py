d6fc50662d92ac8df6f0adca33492dda3e3736b1
Vahndi Minah <vahndi.minah@gmail.com>
2016-01-05 10:58:31 -0500

    Updated SKlearnElement precedingElements to ABCOutputsDataFrame

--- flow_elements/sklearn/v0_17/sklearn_element.py
+++ flow_elements/sklearn/v0_17/sklearn_element.py
@@ -2,12 +2,12 @@
 from models_views.flowElement import FlowElement
 
 # Preceding elements
-from flow_elements.pandas.v0_17_1.ReadDataFrame import ReadDataFrame_Model
-from flow_elements.pandas.v0_17_1.SelectColumns import SelectColumns_Model
+from flow_elements.pandas.v0_17_1.base.ABCs import ABCOutputsDataFrame
+
 
 
 class SKLearnElement(FlowElement):
     
     doc_root = 'http://scikit-learn.org/stable/modules/generated/'
-    precedingElements = [ReadDataFrame_Model, SelectColumns_Model]
+    precedingElements = [ABCOutputsDataFrame]
     
