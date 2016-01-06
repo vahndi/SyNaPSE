11d05e7ce34eac2fc3b2819397a16197a150bcca
vahndi <vahndi.minah@gmail.com>
2016-01-06 11:37:10 -0500

    Added max_cols property to DataFrame display

--- models_views/models.py
+++ models_views/models.py
@@ -23,8 +23,13 @@ def getModels(package_path, sys_path):
     model_files = getFileNames(sys_path, endsWith = '.enaml')
     for model_file in model_files:
         model_name = model_file.split('.')[0]
-        model_list.append(locate('%s.%s.%s_Model' 
-                                 % (package_path, model_name, model_name))) 
+        print 'Loading element: %s...' % model_name
+        try:
+            model_list.append(locate('%s.%s.%s_Model' 
+                                     % (package_path, model_name, model_name)))
+        except:
+            print '!!! Error !!!'
+            
     return model_list
     
 
