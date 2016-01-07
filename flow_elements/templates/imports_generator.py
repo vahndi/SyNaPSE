a5339a51c8d8d5e26c9084e20aa85a6749e70914
vahndi <vahndi.minah@gmail.com>
2016-01-06 19:24:28 -0500

    Added Columnwise Covariance model

--- flow_elements/templates/imports_generator.py
+++ flow_elements/templates/imports_generator.py
@@ -25,26 +25,30 @@ def getImportsCode(element_name, dataframe):
     # ---- 
     atom_types = {'Atom'}
     for widget_type in widget_types:
-        atom_types = atom_types.union(widgets_atoms[widget_type])    
-
+        atom_types = atom_types.union(widgets_atoms[widget_type])  
+    if dataframe['Widget Args'].str.contains('optional').any():
+        atom_types = list(set(list(atom_types) + ['Bool']))
     importsCode = '# Atom\n'
     importsCode += 'from atom.api import %s\n\n' \
                    % ', '.join(sorted(atom_types))
 
     # Enaml    
     # -----
-    standard_widgets = ', '.join(sorted(['Form', 'GroupBox', 'Label', 
-                                         'Notebook', 'Page'] + 
-                                        [e for e in widget_types 
-                                         if e not in custom_widgets
-                                         and e not in fields_widgets]))    
+    standard_widgets = ['Form', 'GroupBox', 'Label', 'Notebook', 'Page'] + \
+                       [e for e in widget_types 
+                        if e not in custom_widgets
+                        and e not in fields_widgets]
+    if dataframe['Widget Args'].str.contains('optional').any():
+        standard_widgets = set(standard_widgets + ['CheckBox'])
+    str_standard_widgets = ', '.join(sorted(standard_widgets))
+    
     importsCode += '\n# Enaml\n'
     # conditional
     conditions = dataframe['Visibility Condition']
     if not conditions.isnull().all():
         importsCode += 'from enaml.core.api import Conditional\n'
     # standard
-    importsCode += 'from enaml.widgets.api import %s\n' % standard_widgets
+    importsCode += 'from enaml.widgets.api import %s\n' % str_standard_widgets
     # fields
     reqd_field_widgets = set(widget_types).intersection(fields_widgets)
     if len(reqd_field_widgets) > 0:
