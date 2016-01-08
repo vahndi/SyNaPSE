425016d7cdf0723330f7d633188a7a4e8148a082
vahndi <vahndi.minah@gmail.com>
2016-01-08 17:51:02 -0500

    Added Expanding Window Statistics models

---
+++ flow_elements/pandas/v0_17_1/ExpandingWindowStatistics/_base/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan  8 17:17:03 2016
+
+@author: vahndi
+"""
+
--- flow_elements/templates/element_generator.py
+++ flow_elements/templates/element_generator.py
@@ -110,8 +110,8 @@ def inspect_element_args(pickle_name, module_name = ''):
 
 if __name__ == '__main__':
 
-    generate_element(spreadsheet_name = 'pandas.DataFrame',
-                     element_name = 'ReIndexToDateRange')
+    generate_element(spreadsheet_name = 'pandas.stats.moments',
+                     element_name = 'ExpandingStatistic')
 
 #    inspect_element_args(pickle_name = 'sklearn_scrape',
 #                         module_name = 'sklearn.linear_model')
