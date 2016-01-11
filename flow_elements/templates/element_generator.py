6f383bf144138ec730eb049f4889d7bb5bd7096d
vahndi <vahndi.minah@gmail.com>
2016-01-11 18:21:10 -0500

    Added more sklearn generator models

--- flow_elements/templates/element_generator.py
+++ flow_elements/templates/element_generator.py
@@ -111,7 +111,7 @@ def inspect_element_args(pickle_name, module_name = ''):
 if __name__ == '__main__':
 
     generate_element(spreadsheet_name = 'sklearn.samples_generator',
-                     element_name = 'MakeBlobs')
+                     element_name = 'Make1')
 
 #    inspect_element_args(pickle_name = 'sklearn_scrape',
 #                         module_name = 'sklearn.linear_model')
