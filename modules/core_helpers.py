834cafd3310dbfb53f6ba851d9a02e029ed64afe
vahndi <vahndi.minah@gmail.com>
2016-01-26 14:19:56 -0500

    Added helpers for getting attributes and DataFrame Blocks model

--- modules/core_helpers.py
+++ modules/core_helpers.py
@@ -1,5 +1,5 @@
 import numpy as np
-from pandas import Series, Index
+from pandas import DataFrame, Index, Series
 
 
 
@@ -12,6 +12,87 @@ def get_public_attr_names(obj):
             and not callable(getattr(obj, a))]
 
 
+def _iterable_to_series(name, iterable):
+    
+    return Series(iterable, name = name)
+
+    
+def _items_to_series(name, item_names, item_values):
+    
+    return Series(data = item_values,
+                  name = 'value',
+                  index = Index(data = item_names,
+                                name = name))
+
+
+# Build a list of type-names and associated types
+
+
+def _group_single_values(dictionary):
+    '''
+    Takes a dict and groups all items of type bool, float, int or str into 
+    separate lists
+    '''
+    item_type_lists = {'bool': [bool, np.bool],
+                       'float': [float, np.float32, np.float64],
+                       'int': [int, np.int32, np.int64],
+                       'str': [str]} 
+    new_dict = {}
+    
+    # Group item types and add to a new dict as a Series
+    for item_type_name in item_type_lists.keys():
+        k_list = []
+        v_list = []
+        for k, v in dictionary.items():
+            if type(v) in item_type_lists[item_type_name]:
+                k_list.append(k)
+                v_list.append(v)
+        if k_list:
+            new_dict[item_type_name + 's'] = _items_to_series(
+                                                 name = item_type_name,
+                                                 item_names = k_list,
+                                                 item_values = v_list)
+
+    # Add other types directly to the new dict
+    for k, v in dictionary.items():
+        if type(v) in (DataFrame, dict, list, np.ndarray, Series, tuple):
+            new_dict[k] = v
+        
+    return new_dict
+
+
+def _dict_repr(dictionary):
+    '''
+    Takes a dict of values or dicts and returns a representation suitable for
+    display.
+    '''
+    grouped_dict = _group_single_values(dictionary)
+    
+    new_dict = {}
+    for key in grouped_dict.keys():
+        
+        value = grouped_dict[key]
+        value_type = type(value)
+
+        if ((value_type in (list, Series, tuple)) or
+            (value_type is np.ndarray and value.ndim == 1)):
+            
+            new_dict[key] = _iterable_to_series(name = key,
+                                              iterable = value)
+        
+        elif (value_type is DataFrame or 
+              (value_type is np.ndarray and value.ndim == 2)):
+                  
+            new_dict[key] = DataFrame(value)
+        
+        elif value_type is dict:
+            
+            new_dict[key] = {_dict_repr(value[k])
+                             for k in value.keys()} 
+        
+    return new_dict
+
+
 def public_attrs_to_dict(obj):
     '''
     Takes an object `obj`'s public attributes and splits them up into different
@@ -19,63 +100,18 @@ def public_attrs_to_dict(obj):
     values are series with the attribute names as keys and the attribute values
     as values.
     '''
-    # Build a list of type-names for the output and associated types
-    item_type_lists = {'bool': [bool, np.bool],
-                       'float': [float, np.float32, np.float64],
-                       'int': [int, np.int32, np.int64],
-                       'str': [str]} 
-    iterable_type_lists = {'list': [list],
-                           'ndarray': [np.ndarray],
-                           'tuple': [tuple]}
-    dict_type_lists = {'dict': [dict]}
-
     # Get all the attrs from the object
     obj_attrs = {attr_name: getattr(obj, attr_name) 
                  for attr_name in get_public_attr_names(obj)}
-    
-    attr_dict = {}
-
-    # item types
-    for item_type in item_type_lists.keys():
-        # Get all the attriutes of that type, if any
-        type_attr_names = [attr_name for attr_name in obj_attrs.keys()
-                           if type(obj_attrs[attr_name]) 
-                           in item_type_lists[item_type]]
-        num_attrs = len(type_attr_names)
-        if num_attrs > 0:
-            # Create a series of the values and add it to the attribute dict
-            attr_dict[item_type + 's'] = Series(
-                                           data = [obj_attrs[a] 
-                                                   for a in type_attr_names],
-                                           name = 'value',
-                                           index = Index(
-                                             [a for a in type_attr_names],
-                                             name = item_type))
-
-    # iterable types
-    for iterable_type in iterable_type_lists.keys():
-        # Get all the attributes of that type, if any
-        type_attr_names = [attr_name for attr_name in obj_attrs.keys()
-                           if type(obj_attrs[attr_name]) 
-                           in iterable_type_lists[iterable_type]]
-        if len(type_attr_names) > 0:
-
-            # Create a series of the values and add it to the attribute dict
-            attr_dict[iterable_type + 's'] = {type_attr_name:
-                                              Series(obj_attrs[type_attr_name],
-                                                     name = type_attr_name)
-                                        for type_attr_name in type_attr_names}
-
-    return attr_dict
+    return _dict_repr(obj_attrs)    
     
 
 if __name__ == '__main__':
     
     import seaborn as sns
     iris = sns.load_dataset('iris')
-    d = public_attrs_to_dict(iris.columns)
-    for k in d.keys():
-        print k, d[k]
+    d = public_attrs_to_dict(iris.blocks)
+    print d
 
 
     
---
+++ modules/series_figures.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26 14:04:43 2016
+
+@author: vahndi
+"""
+
