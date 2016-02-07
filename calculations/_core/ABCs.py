d79cc421f923e8e53eb490cc341ecde71470f184
vahndi <vahndi.minah@gmail.com>
2016-02-07 13:53:19 -0500

    Added Series Statistics calcs and separated Quantile from Quantiles - need to check all these

--- calculations/_core/ABCs.py
+++ calculations/_core/ABCs.py
@@ -2,88 +2,72 @@ from models_views.calculation import Calculation_Model
 
 
 
+# Outputs
+# -------
+class ABCOutputsValue(Calculation_Model):
+    pass
+
+class ABCOutputsBool(ABCOutputsValue):    
+    pass
+
 class ABCOutputsDataFrame(Calculation_Model):
-    
+    pass
+
+class ABCOutputsFloat(ABCOutputsValue):
+    pass
+
+class ABCOutputsIndex(Calculation_Model):    
+    pass
+
+class ABCOutputsInt(ABCOutputsValue):    
+    pass
+
+class ABCOutputsSeries(ABCOutputsValue):    
     pass
 
 
 
+# Takes
+# -----
 class ABCTakesDataFrame(Calculation_Model):
-
     preceding_calcs = [ABCOutputsDataFrame]
 
-
-
-class ABCOutputsSeries():
-    
-    pass
-
-
-
-class ABCTakesSeries(Calculation_Model):
-    
-    preceding_calcs = [ABCOutputsSeries]
-
-
-
-class ABCTakes2Series(Calculation_Model):
-    
-    preceding_calcs = [ABCOutputsSeries, 
-                       ABCOutputsSeries]
-
-
-
-class ABC2SeriesToSeries(ABCTakes2Series,
-                         ABCOutputsSeries):
-    
-    pass
-
-
-
-class ABCOutputsIndex(Calculation_Model):
-    
-    pass
-
-
-
-class ABCTakesIndex(Calculation_Model):
-    
+class ABCTakesIndex(Calculation_Model):    
     preceding_calcs = [ABCOutputsIndex]
 
+class ABCTakesSeries(Calculation_Model):    
+    preceding_calcs = [ABCOutputsSeries]
+
+class ABCTakes2Series(Calculation_Model):
+    preceding_calcs = [ABCOutputsSeries, ABCOutputsSeries]
 
 
-class ABCDataFrameToIndex(ABCTakesDataFrame,
-                          ABCOutputsIndex):
 
+# To
+# --
+class ABCDataFrameToDataFrame(ABCTakesDataFrame, ABCOutputsDataFrame):
     pass
 
-
-
-class ABCDataFrameToDataFrame(ABCTakesDataFrame, 
-                              ABCOutputsDataFrame):
-    
+class ABCDataFrameToIndex(ABCTakesDataFrame, ABCOutputsIndex):
     pass
 
-
-
-class ABCDataFrameToDataFrameOrSeries(ABCTakesDataFrame, 
-                                      ABCOutputsDataFrame,
-                                      ABCOutputsSeries):
-    
+class ABCDataFrameToSeries(ABCTakesDataFrame, ABCOutputsSeries):    
     pass
 
-
-
-class ABCDataFrameToSeries(ABCTakesDataFrame, 
-                           ABCOutputsSeries):
-    
+class ABCSeriesToBool(ABCTakesSeries, ABCOutputsBool):                          
     pass
 
-
-
-class ABCSeriesToSeries(ABCTakesSeries,
-                        ABCOutputsSeries):
-    
+class ABCSeriesToInt(ABCTakesSeries, ABCOutputsInt):                 
     pass
 
+class ABCSeriesToFloat(ABCTakesSeries, ABCOutputsFloat):
+    pass
 
+class ABCSeriesToSeries(ABCTakesSeries, ABCOutputsSeries):
+    pass
+
+class ABCSeriesToValue(ABCTakesSeries, ABCOutputsValue):
+    pass
+
+class ABC2SeriesToSeries(ABCTakes2Series, ABCOutputsSeries):
+    pass
