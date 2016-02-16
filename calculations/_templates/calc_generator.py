0b6d3d1a3bf7eea9fd35aacb9913c23934f59442
vahndi <vahndi.minah@gmail.com>
2016-02-16 15:44:04 -0500

    Added String split calcs

--- calculations/_templates/calc_generator.py
+++ calculations/_templates/calc_generator.py
@@ -110,8 +110,8 @@ def inspect_calc_args(pickle_name, module_name = ''):
 
 if __name__ == '__main__':
 
-    generate_calc(spreadsheet_name = 'sklearn.preprocessing',
-                  calc_name = 'RobustScaler')
+    generate_calc(spreadsheet_name = 'pandas.Series.str',
+                  calc_name = 'Split')
 
 #    inspect_calc_args(pickle_name = 'sklearn_scrape',
 #                         module_name = 'sklearn.linear_model')
